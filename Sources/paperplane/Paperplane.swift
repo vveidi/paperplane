d143fec6d7280b2cffce07f6f32971d8c84eefa0
Vadim Burkin <mailboxofvadim@gmail.com>
2025-06-26 23:32:00 +0300

    ref: errors and help command

--- Sources/paperplane/Paperplane.swift
+++ Sources/paperplane/Paperplane.swift
@@ -4,9 +4,20 @@ import ArgumentParser
 struct Paperplane: ParsableCommand {
     static var configuration: CommandConfiguration {
         CommandConfiguration(
-            abstract: "Утилита для работы с книгами",
-            subcommands: [SendBookCommand.self],
-            defaultSubcommand: SendBookCommand.self
+            commandName: "send-book",
+            abstract: "Sends a book file or folder as an email attachment.",
+            discussion: """
+                Usage example:
+                  paperplane send-book --sender you@email.com --receiver kindle@kindle.com --path /path/to/book.mobi
+
+                Parameters:
+                  --sender       Email address of the sender.
+                  --receiver     Email address of the recipient.
+                  --path         Path to the book file or folder.
+                  --remove-after-send  Remove file or folder after sending.
+
+                Supported formats: mobi, azw, azw3, epub, pdf, and more.
+                """
         )
     }
 }
